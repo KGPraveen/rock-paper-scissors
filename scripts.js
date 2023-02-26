cb8bd552b8b5de67f4c6637800b92e44dc232c46
Praveen K.G <kgpraveen2904@gmail.com>
2023-02-26 10:19:14 +0530

    Added full RPS to the Script
    Added full Rock Paper Scissors game to the script with 5 rounds to determine the winner. Tie games won't count to the round.

--- scripts.js
+++ scripts.js
@@ -1,69 +1,71 @@
 
 
-function getComputerChoice() {
-    let computerChoice = ["ROCK", "PAPER", "SCISSORS"]
-    return (computerChoice[Math.floor(Math.random() * 3)])
+let computerScore = 0;
+let userScore = 0;
+
+for (let round = 1; round <= 5; round++) {
+    let userChoice = prompt("Enter your choice [Rock/Paper/Scissors]: ")
+    userChoice = userChoiceCapitalizer(userChoice);
+    switch (userChoice) {
+        case 0: console.log("ROCK")
+            break;
+        case 1: console.log("PAPER")
+            break;
+        case 2: console.log("SCISSORS")
+            break;
+        case 3: console.log("WRONG INPUT!")
+            break;
+    }
+    let computerChoice = computerChoiceFunction();
+
+    if (userChoice == computerChoice) {
+        console.log("The Computer Input was same as yours, so it's a Tie.\n\n");
+        round--;
+    } else if (userChoice == 0 && computerChoice == 1) {
+        console.log("Computer Input -> PAPER\nYou Lose!\n\n");
+        computerScore += 1;
+    } else if (userChoice == 0 && computerChoice == 2) {
+        console.log("Computer Input -> SCISSORS\nYou Win!\n\n");
+        userScore += 1;
+    } else if (userChoice == 1 && computerChoice == 0) {
+        console.log("Computer Input -> ROCK\nYou win!\n\n");
+        userScore += 1;
+    } else if (userChoice == 1 && computerChoice == 2) {
+        console.log("Computer Input -> SCISSORS\nYou lose!\n\n");
+        computerScore += 1;
+    } else if (userChoice == 2 && computerChoice == 0) {
+        console.log("Computer Input -> ROCK\nYou Lose!\n\n");
+        computerScore += 1;
+    } else if (userChoice == 2 && computerChoice == 1) {
+        console.log("Computer Input -> PAPER\nYou Win!\n\n");
+        userScore += 1;
+    } else {
+        console.log("Hmm, something is wrong...\n\n");
+    }
 }
 
-function playRound(playerSelection, computerSelection) {
-
-    playerSelection = playerSelection.toUpperCase()
-
-    if (playerSelection == "ROCK" && computerSelection == "ROCK") {
-        return ("It's a draw! Two Rocks")
-    }
-    else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
-        return ("You Lose! Paper beats Rock")
-    }
-    else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
-        return ("You Win! Rock beats Scissors")
-    }
-    else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
-        return ("You Win! Paper beats Rock")
-    }
-    else if (playerSelection == "PAPER" && computerSelection == "PAPER") {
-        return ("It's a draw! Two Papers")
-    }
-    else if (playerSelection == "PAPER" && computerSelection == "SCISSORS") {
-        return ("You Lose! Paper beats Rock")
-    }
-    else if (playerSelection == "SCISSORS" && computerSelection == "ROCK") {
-        return ("You Lose! Rock beats Scissors")
-    }
-    else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
-        return ("You Win! Scissors beats Paper")
-    }
-    else if (playerSelection == "SCISSORS" && computerSelection == "SCISSORS") {
-        return ("It's a draw! Two Scissors")
-    }
-    else {
-        return ("Invalid Choice! Enter Rock/Paper/Scissors.")
-    }
-
-}
-
-function game() {
-    var playerScore = 0, computerScore = 0;
-
-    for (let round = 1; round <= 5; round++) {
-        let playerChoice = prompt("Enter Rock/Paper/Scissors:")
-        let computerChoice = getComputerChoice()
-        let result = playRound(playerChoice, computerChoice)
-        console.log(result);
-
-        if (result.includes("Win")) {
-            playerScore++
-        } else if (result.includes("Lose")) {
-            computerScore++
-        } else if (result.includes("Invalid")) {
-            round = round - 1
-        } else {
-            round = round - 1
-        }
-    }
-
-    console.log(`Your score: ${playerScore}\nComputer Score: ${computerScore}`);
+if (userScore > computerScore) {
+    console.log("YOU WON THE GAME!");
+} else if (computerScore > userScore) {
+    console.log("YOU LOST THE GAME...");
+} else {
+    console.log("THE GAME WAS A TIE...");
 }
 
 
-game() //And so it begins!
+
+function computerChoiceFunction() {
+    return (Math.floor(Math.random() * 2));
+}
+
+function userChoiceCapitalizer(userChoice) {
+    if (userChoice.toUpperCase() === "ROCK") {
+        return 0
+    } else if (userChoice.toUpperCase() === "PAPER") {
+        return 1
+    } else if (userChoice.toUpperCase() === "SCISSORS") {
+        return 2
+    } else {
+        return 3
+    }
+}
