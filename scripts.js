a2e6c910e46fb856ce496160b577dc249d5729f6
Praveen K.G <kgpraveen2904@gmail.com>
2023-02-27 23:45:13 +0530

    Completely Overhauled RPS
    Added full HTML, CSS and JS functionalities, from music to sound to background video to transitions.

--- scripts.js
+++ scripts.js
@@ -1,71 +1,114 @@
 
+let userScore = document.querySelector(".playerscore");
+let computerScore = document.querySelector(".computerscore");
+var isplaying = 0;
 
-let computerScore = 0;
-let userScore = 0;
-
-for (let round = 1; round <= 5; round++) {
-    let userChoice = prompt("Enter your choice [Rock/Paper/Scissors]: ")
-    userChoice = userChoiceCapitalizer(userChoice);
-    switch (userChoice) {
-        case 0: console.log("ROCK")
-            break;
-        case 1: console.log("PAPER")
-            break;
-        case 2: console.log("SCISSORS")
-            break;
-        case 3: console.log("WRONG INPUT!")
-            break;
-    }
-    let computerChoice = computerChoiceFunction();
-
-    if (userChoice == computerChoice) {
-        console.log("The Computer Input was same as yours, so it's a Tie.\n\n");
-        round--;
-    } else if (userChoice == 0 && computerChoice == 1) {
-        console.log("Computer Input -> PAPER\nYou Lose!\n\n");
-        computerScore += 1;
-    } else if (userChoice == 0 && computerChoice == 2) {
-        console.log("Computer Input -> SCISSORS\nYou Win!\n\n");
-        userScore += 1;
-    } else if (userChoice == 1 && computerChoice == 0) {
-        console.log("Computer Input -> ROCK\nYou win!\n\n");
-        userScore += 1;
-    } else if (userChoice == 1 && computerChoice == 2) {
-        console.log("Computer Input -> SCISSORS\nYou lose!\n\n");
-        computerScore += 1;
-    } else if (userChoice == 2 && computerChoice == 0) {
-        console.log("Computer Input -> ROCK\nYou Lose!\n\n");
-        computerScore += 1;
-    } else if (userChoice == 2 && computerChoice == 1) {
-        console.log("Computer Input -> PAPER\nYou Win!\n\n");
-        userScore += 1;
+function togglemusic() {
+    let music = document.querySelector(".music");
+    
+    if (isplaying) {
+        music.pause();
+        isplaying = 0;
     } else {
-        console.log("Hmm, something is wrong...\n\n");
+        music.play();
+        isplaying = 1;
     }
 }
 
-if (userScore > computerScore) {
-    console.log("YOU WON THE GAME!");
-} else if (computerScore > userScore) {
-    console.log("YOU LOST THE GAME...");
-} else {
-    console.log("THE GAME WAS A TIE...");
+function clicked(icon) {
+
+    if (parseInt(userScore.innerHTML) === 5 || parseInt(computerScore.innerHTML) === 5) {
+        return;
+    }
+
+    let audio = document.querySelector(`.clickaudio`);
+    audio.currentTime = 0;
+    audio.play();
+    let img = document.querySelector("#" + icon);
+    img.classList.add(`${icon}IconClicked`);
+    img.classList.remove("iconRaised");
+    game(icon);
 }
 
+let imgs = Array.from(document.querySelectorAll("img"));
+imgs.forEach(img => img.addEventListener("transitionend", reverseNow));
 
+function reverseNow(e) {
+    if (e.propertyName == "transform") {
+        e.target.classList.add("iconRaised");
+        e.target.classList.remove("iconClicked");
+    }
+}
 
 function computerChoiceFunction() {
-    return (Math.floor(Math.random() * 2));
+    let randomChoice = (Math.floor(Math.random() * 2));
+
+    if (randomChoice == 0) {
+        return "rock";
+    } else if (randomChoice == 1) {
+        return "paper";
+    } else if (randomChoice == 2) {
+        return "scissors";
+    } else {
+        return "error!";
+    }
 }
 
-function userChoiceCapitalizer(userChoice) {
-    if (userChoice.toUpperCase() === "ROCK") {
-        return 0
-    } else if (userChoice.toUpperCase() === "PAPER") {
-        return 1
-    } else if (userChoice.toUpperCase() === "SCISSORS") {
-        return 2
+function game(userChoice) {
+
+    let computerChoice = computerChoiceFunction();
+    let gamestatus = document.querySelector(".gamestatus");
+
+    if (computerChoice == "error!") {
+        console.log("My brother! the computer choice function returned an error!");
+        return;
+    }
+
+    if (userChoice == computerChoice) {
+
+        gamestatus.innerHTML = "The Computer Input was same as yours.<br><span class='roundtie'>So it's a Tie.</span>";
+
+    } else if (userChoice == "rock" && computerChoice == "paper") {
+
+        gamestatus.innerHTML = "Computer Input -> PAPER.<br><span class='roundlost'>You lost this round.</span>";
+        computerScore.innerHTML = parseInt(computerScore.innerHTML) + 1;
+
+    } else if (userChoice == "rock" && computerChoice == "scissors") {
+
+        gamestatus.innerHTML = "Computer Input -> SCISSORS.<br><span class='roundwon'>You won this round!</span>";
+        userScore.innerHTML = parseInt(userScore.innerHTML) + 1;
+
+    } else if (userChoice == "paper" && computerChoice == "rock") {
+
+        gamestatus.innerHTML = "Computer Input -> ROCK.<br><span class='roundwon'>You won this round!</span>";
+        userScore.innerHTML = parseInt(userScore.innerHTML) + 1;
+
+    } else if (userChoice == "paper" && computerChoice == "scissors") {
+
+        gamestatus.innerHTML = "Computer Input -> SCISSORS.<br><span class='roundlost'>You lost this round.</span>";
+        computerScore.innerHTML = parseInt(computerScore.innerHTML) + 1;
+
+    } else if (userChoice == "scissors" && computerChoice == "rock") {
+
+        gamestatus.innerHTML = "Computer Input -> ROCK.<br><span class='roundlost'>You lost this round.</span>";
+        computerScore.innerHTML = parseInt(computerScore.innerHTML) + 1;
+
+    } else if (userChoice == "scissors" && computerChoice == "paper") {
+
+        gamestatus.innerHTML = "Computer Input -> PAPER.<br><span class='roundwon'>You won this round!</span>";
+        userScore.innerHTML = parseInt(userScore.innerHTML) + 1;
+
     } else {
-        return 3
+
+        gamestatus.innerHTML = "Hmm, something is wrong...";
+
+    }
+
+    if (userScore.innerHTML == 5) {
+        gamestatus.innerHTML = "CONGRATULATIONS!<br>YOU WON THE GAME!<br><span class='gamewonspan'>Refresh Page to Restart Game</span>";
+        gamestatus.classList.add("gamewon");
+    } else if (computerScore.innerHTML == 5) {
+        gamestatus.innerHTML = "Sorry, but you lost the Game...<br><span class='gamelostspan'>Refresh Page to Restart Game</span>";
+        gamestatus.classList.add("gamelost");
     }
 }
